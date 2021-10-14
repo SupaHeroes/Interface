7ba9066c694053b6d72427e71891c62f29d70c90
Daniel Helm <dan.helm@gmail.com>
2021-10-14 13:05:54 -0500

    feat: Add SPA fallback

--- svelte.config.js
+++ svelte.config.js
@@ -3,30 +3,32 @@ import adapter from '@sveltejs/adapter-static';
 import path from 'path';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [preprocess({
-        postcss: true
-    })],
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
 
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
-		target: '#svelte',
-		vite: {
-			resolve: {
-				alias: {
-					'@comp': path.resolve('./src/components'),
-					'@lib': path.resolve('./src/lib'),
-				}
-			}
-		}
-	}
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    adapter: adapter({
+      // default options are shown
+      pages: 'build',
+      assets: 'build',
+      fallback: '200.html',
+    }),
+    target: '#svelte',
+    vite: {
+      resolve: {
+        alias: {
+          '@comp': path.resolve('./src/components'),
+          '@lib': path.resolve('./src/lib'),
+        },
+      },
+    },
+  },
 };
 
 export default config;
